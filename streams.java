a9ac077a75ae4dc0e4357dc1be47cc99f80ce0fa
shresth-sahai <shresth.sahai@salesforce.com>
2026-01-03 11:46:49 +0530

    streams

--- streams.java
+++ streams.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class streams {
     
@@ -22,15 +24,41 @@ lazy in nature only executed when we require them
 
 */
 
-List<Integer> salaryList= new ArraytList<>();
+// List<Integer> salaryList= new ArraytList<>();
 
-salaryList.add(400);
-salaryList.add(4000);
-salaryList.add(100);
+// salaryList.add(400);
+// salaryList.add(4000);
+// salaryList.add(100);
 
-// [400,4000,100]
+// // [400,4000,100]
 
-long output =salaryList.stream().filter((Integer val)-> val>100).count();
+// long output =salaryList.stream().filter((Integer val)-> val>100).count();
 
-System.out.print(output);
+// System.out.print(output);
+
+
+
+// different ways to create a java stream 
+// List <Integer> l=new Arrays.asList(1,2,3,4);
+
+// Stream<Integer> streamforList=l.stream();
+
+// Integer[] salaryArray={1,2,2};
+// Stream<Integer> streamfor=salaryArray.stream();
+
+
+// Stream<Integer> streamFromStaticMethod=Stream.of(100,200,2000);
+// // stream builder 
+
+// Stream.Builder<Integer> streamBuilder=Stream.builder();
+// streamBuilder.add(100).add(122).add(234);
+// Stream<Integer> streamfromBuilder=streamBuilder.build();
+
+// // stream iterate 
+// Stream<Integer> streamFromIterate= Stream.iterate(1000,(Integer,n)-> n+400)
+// .limit(5);
+
+ 
+
+
 
